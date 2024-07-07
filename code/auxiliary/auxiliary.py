e058323292ba0a9b2d288bc9afa28b608a459ba6
Ruzejjur <ruzheinikau@email.cz>
2024-07-07 10:55:50 +0200

    Partialy vectorised expert generation of expert weight tables and vectorised trust calculation.

--- code/auxiliary/auxiliary.py
+++ code/auxiliary/auxiliary.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 
 def generate_primary_modeler_weights(primary_modeler_scores, opinion_certainty_array, score_range, initial_feature_weight):
     primary_modeler_scores = np.atleast_2d(np.array(primary_modeler_scores))
@@ -27,20 +27,13 @@ def generate_expert_weights(expert_scores, score_range, initial_feature_weight):
     expert_scores = np.atleast_2d(np.array(expert_scores))
     weight_table = np.full((expert_scores.shape[1], score_range, expert_scores.shape[0]), initial_feature_weight)
     
-    for i in range(expert_scores.shape[0]):
-        for j in range(expert_scores.shape[1]):
-            if expert_scores[i, j] != 0:
-                score = expert_scores[i, j] - 1  # Adjust for 0-based indexing
-                weight_table[j, score, 0] += 1
-    
     non_zero_scores = expert_scores != 0
     
-    scores = expert_scores[non_zero_scores] - 1
+    j_indices = np.where(expert_scores[non_zero_scores])
     
-    j_indices = np.where(scores)
+    scores = expert_scores[non_zero_scores] - 1 # Adjust for zero based indexing
     
     weight_table[j_indices, scores, 0] += 1
-
 
     return weight_table
 
@@ -78,39 +71,46 @@ def simulated_example(primary_modeler_scores, opinion_certainty_array, apply_cer
     # Generating primary modelers weights
     primary_modeler_weights = generate_primary_modeler_weights(primary_modeler_scores, opinion_certainty_array, score_range, initial_feature_weight)
 
-    samsung_expert_opinion_weights = np.zeros((3, score_range, 10))
-    for i in range(10):
-        expert_opinion_weight_table = generate_expert_weights(expert_opinions[i], score_range, 0)
-        for j in range(3):
-            for k in range(6):
-                samsung_expert_opinion_weights[j, k, i] = expert_opinion_weight_table[j,k]
-    
-    iphone_expert_opinion_weights = np.zeros((3, score_range, 10))
-    for i in range(10,20):
-        expert_opinion_weight_table = generate_expert_weights(expert_opinions[i], score_range, 0)
-        for j in range(3):
-            for k in range(6):
-                iphone_expert_opinion_weights[j, k, i-10] = expert_opinion_weight_table[j,k]
+    # Generate expert opinion weights for samsung experts
+    # Note 1: Watch out for data type, needs to be float otherwise trust is not applied correctly
+    # TODO: In the future, look into more efficient container, as the float 32 array would take up to much space
+    # TODO: Fully vectorise this operation
+    samsung_expert_opinion_weights = np.array([generate_expert_weights(expert_opinions[i], score_range, 0) for i in range(10)], dtype=np.float32)
 
-    xiaomi_expert_opinion_weights = np.zeros((3, score_range, 10))
-    for i in range(20,30):
-        expert_opinion_weight_table = generate_expert_weights(expert_opinions[i], score_range, 0)
-        for j in range(3):
-            for k in range(6):
-                xiaomi_expert_opinion_weights[j, k, i-20] = expert_opinion_weight_table[j,k]
+    samsung_expert_opinion_weights = np.squeeze(samsung_expert_opinion_weights, axis=-1)
+    samsung_expert_opinion_weights = np.transpose(samsung_expert_opinion_weights, (1,2,0))
 
-    samsung_expert_opinion_weights_trust = np.zeros_like(samsung_expert_opinion_weights)
-    for i in range(samsung_expert_opinion_weights.shape[2]):
-        samsung_expert_opinion_weights_trust[:,:,i] = samsung_expert_opinion_weights[:,:,i]*trust_matrix[0,i]
-    
-    iphone_expert_opinion_weights_trust = np.zeros_like(iphone_expert_opinion_weights)
-    for i in range(iphone_expert_opinion_weights.shape[2]):
-        iphone_expert_opinion_weights_trust[:,:,i] = iphone_expert_opinion_weights[:,:,i]*trust_matrix[1,i]
-    
-    xiaomi_expert_opinion_weights_trust = np.zeros_like(xiaomi_expert_opinion_weights)
-    for i in range(xiaomi_expert_opinion_weights.shape[2]):
-        xiaomi_expert_opinion_weights_trust[:,:,i] = xiaomi_expert_opinion_weights[:,:,i]*trust_matrix[2,i]
+    # Generate expert opinion weights for iphone experts
+    # Note 1: Watch out for data type, needs to be float otherwise trust is not applied correctly
+    # TODO: In the future, look into more efficient container, as the float 32 array would take up to much space
+    # TODO: Fully vectorise this operation
+    iphone_expert_opinion_weights = np.array([generate_expert_weights(expert_opinions[i], score_range, 0) for i in range(10,20)], dtype=np.float32)
 
+    iphone_expert_opinion_weights = np.squeeze(iphone_expert_opinion_weights, axis=-1)
+    iphone_expert_opinion_weights = np.transpose(iphone_expert_opinion_weights, (1,2,0))
+                
+    # Generate expert opinion weights for iphone experts
+    # Note 1: Watch out for data type, needs to be float otherwise trust is not applied correctly
+    # TODO: In the future, look into more efficient container, as the float 32 array would take up to much space
+    # TODO: Fully vectorise this operation
+    xiaomi_expert_opinion_weights = np.array([generate_expert_weights(expert_opinions[i], score_range, 0) for i in range(20,30)], dtype=np.float32)
+
+    xiaomi_expert_opinion_weights = np.squeeze(xiaomi_expert_opinion_weights, axis=-1)
+    xiaomi_expert_opinion_weights = np.transpose(xiaomi_expert_opinion_weights, (1,2,0))
+
+    # TODO: Replace the shape call with number of experts input into the function
+    i_indices =  np.arange(samsung_expert_opinion_weights.shape[2])
+    samsung_expert_opinion_weights_trust = samsung_expert_opinion_weights[:,:,i_indices] * trust_matrix[0,i_indices]
+   
+    # TODO: Replace the shape call with number of experts input into the function  
+    i_indices =  np.arange(iphone_expert_opinion_weights.shape[2])
+    iphone_expert_opinion_weights_trust = iphone_expert_opinion_weights[:,:,i_indices] * trust_matrix[1,i_indices]
+
+    # TODO: Replace the shape call with number of experts input into the function 
+    i_indices =  np.arange(xiaomi_expert_opinion_weights.shape[2])
+    xiaomi_expert_opinion_weights_trust = xiaomi_expert_opinion_weights[:,:,i_indices] * trust_matrix[2,i_indices]
+    
+    
     cumulative_expert_weights = np.zeros((3, score_range, 3))
     cumulative_expert_weights[:, :, 0] = np.sum(samsung_expert_opinion_weights_trust, axis=2)
     cumulative_expert_weights[:, :, 1] = np.sum(iphone_expert_opinion_weights_trust, axis=2)
